edaa2354e7004f86be0fc256ba5f970ecb7c5fcd
Kara <kara.diggelmann@edu.tbz.ch>
2026-06-03 14:23:41 +0200

    table for menuItem also integrated in module

--- src/main/java/ch/noseryoung/rest_foods/domains/menu/MenuItem/MenuItem.java
+++ src/main/java/ch/noseryoung/rest_foods/domains/menu/MenuItem/MenuItem.java
@@ -1,4 +1,58 @@
 package ch.noseryoung.rest_foods.domains.menu.MenuItem;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "menu_item")
 public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "item_id", nullable = false)
+    @JsonProperty("id")
+    private UUID itemId;
+
+    @NotBlank
+    @Column
+    private String chefsChoice1;
+
+    @NotBlank
+    @Column
+    private String chefsChoice2;
+
+    @NotBlank
+    @Column
+    private String vegetarian1;
+
+    @NotBlank
+    @Column
+    private String vegetarian2;
+
+    @NotBlank
+    @Column
+    private String meat1;
+
+    @NotBlank
+    @Column
+    private String meat2;
+
+    @NotBlank
+    @Column
+    private String fish1;
+
+    @NotBlank
+    @Column
+    private String fish2;
+
 }
