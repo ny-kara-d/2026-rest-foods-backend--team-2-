290c427d5fabae1181012810436797e1c9a858e5
ny-lionel-s <lionel.schultze@noseryoung.ch>
2026-06-04 11:03:01 +0200

    made all CRUD operations for Reservation

--- src/main/java/ch/noseryoung/rest_foods/domains/reservations/reservation/ReservationController.java
+++ src/main/java/ch/noseryoung/rest_foods/domains/reservations/reservation/ReservationController.java
@@ -1,5 +1,7 @@
 package ch.noseryoung.rest_foods.domains.reservations.reservation;
 
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +12,33 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
-    @Autowired ReservationService reservationService;
+    @Autowired
+    ReservationService reservationService;
 
 
     @GetMapping("")
-    ResponseEntity<List<Reservation>> getAllReservations(@RequestParam(required = false) String name){
+    ResponseEntity<List<Reservation>> getAllReservations(@RequestParam(required = false) String name) {
         return ResponseEntity.status(200).body(reservationService.getAllReservations());
     }
 
     @GetMapping("/{reservation_id}")
     ResponseEntity<Reservation> getReservationById(@PathVariable UUID reservation_id) throws Exception {
         return ResponseEntity.status(200).body(reservationService.getReservationbyId(reservation_id));
+    }
+
+    @PostMapping("/")
+    ResponseEntity<Reservation> createReservation(@Valid @RequestBody Reservation reservation) {
+        return ResponseEntity.status(201).body(reservationService.createReservation(reservation));
+    }
+
+    @PutMapping("{reservation_id}")
+    ResponseEntity<Reservation> updateReservation(@PathVariable UUID reservation_id, @Valid @RequestBody Reservation newReservation) throws Exception {
+        return ResponseEntity.status(200).body(reservationService.updateReservation(reservation_id, newReservation));
+    }
+
+    @DeleteMapping("{reservation_id}")
+    void deleteReservation(@PathVariable UUID reservation_id) throws Exception {
+        reservationService.deleteReservation(reservation_id);
     }
 
 
